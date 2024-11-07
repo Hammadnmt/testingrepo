9fc12441894a47f8d745ceb4c2c66808e97345a8
Hammad <hammad.alinmt@gmail.com>
2024-11-07 16:21:42 +0500

    Basic Server & Router Set up

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const app=express()
+
+app.get('/',(req,res)=>{
+    res.send("Hello from")
+})
+app.listen(3000,()=>{
+    console.log(`Server on http://localhost:3000`)
+})
